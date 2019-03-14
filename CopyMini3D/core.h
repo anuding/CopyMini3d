1ae898d6bb71974949680d8ce838b4c6cdd8f515
anu ding <anuding@126.com>
2019-03-14 20:06:37 +0800

    gf

---
+++ CopyMini3D/core.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int InitWindow();
+int CloseWindow();
+void DispatchWindow();
+void UpdateWindow();
---
+++ CopyMini3D/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	//输出内容到控制台用cout,cout不是一个方法而是一个类
+	cout << "HelloWorld" << endl;
+	getchar();
+	return 0;
+}
